96aecf7036a49418b43b93df0874fca8f37daaf1
fabriceniyonkuruishimwe <f.ishimweni@alustudent.com>
2026-06-29 09:00:12 -0700

    Add quest 01 first spell

---
+++ quests/quest_01_first_spell.py
@@ -0,0 +1,6 @@
+# Quest 1: Your First Spell
+# Concept: print() - The command to make the computer talk.
+
+print("Welcome, brave adventurer!")
+print("You have entered the realm of Python.")
+print("May your code be bug-free and your logic sharp!")
